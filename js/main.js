575c637844ec3cc22f45bd751600fedd60ab57b3
Nikita Gukov <isdeltas@gmail.com>
2018-02-04 17:41:54 +0300

    add findCity method

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let city = 'Minsk';
-const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}`;
+
 let info = 'error';
 
 function initMap(location) {
@@ -8,7 +8,7 @@ function initMap(location) {
         lng: location.lng
     };
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
+        zoom: 10,
         center: uluru
     });
     var marker = new google.maps.Marker({
@@ -17,18 +17,27 @@ function initMap(location) {
     });
 }
 
-let generateMap = function () {
+function findCity(){
+    let city = document.getElementById('cityNameField').value;
+    generateMap(city);
+    return false;
+    
+}
+
+
+
+function generateMap(city) {
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}`;
     fetch(url)
         .then((response) => {
             return response.json();
-
         })
         .then((message) => {
             if (message.status == 'OK') {
                 let location = message.results[0].geometry.location;
                 console.log(`${location.lat}  +  ${location.lng}`)
                 return location;
-            } else alert(message.status)
+            } else alert(message.status);
         })
         .then((location) => {
             initMap(location)
