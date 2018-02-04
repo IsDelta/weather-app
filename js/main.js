798dd0321bd20af433b6140f2879cba8bb78c61e
Nikita Gukov <isdeltas@gmail.com>
2018-02-04 18:49:40 +0300

    output info about whether by Geoposition

--- js/main.js
+++ js/main.js
@@ -17,14 +17,41 @@ function initMap(location) {
     });
 }
 
-function findCity(){
+function findCity() {
     let city = document.getElementById('cityNameField').value;
     generateMap(city);
     return false;
-    
+
 }
 
-
+function getWetherByKey(key) {
+    let url = (`http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=aJ7lv4L6tLHDwjVcLDBD7R3G0up5bkGe&language=ru`);
+    var key = 0;
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((message) => {
+            console.log(message[0].WeatherText)
+        })
+        .catch(console.log);
+}
+function getWether() {
+    let lat = 53.90453979999999,
+        lng = 27.5615244;
+    let url1 = (`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=aJ7lv4L6tLHDwjVcLDBD7R3G0up5bkGe&q=${lat},${lng}&language=ru`);
+    fetch(url1)
+        .then((response) => {
+            return response.json();
+        })
+        .then((message) => {
+            return key = message.Key;
+        })
+        .then((key) => {
+            getWetherByKey(key)
+        })
+        .catch(console.log)
+}
 
 function generateMap(city) {
     let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}`;
@@ -36,12 +63,8 @@ function generateMap(city) {
             if (message.status == 'OK') {
                 let location = message.results[0].geometry.location;
                 console.log(`${location.lat}  +  ${location.lng}`)
-                return location;
+                initMap(location);
             } else alert(message.status);
-        })
-        .then((location) => {
-            initMap(location)
-            return;
         })
         .catch(console.log)
 }
